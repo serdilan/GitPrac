1f84fd9aaf0c6c3527c537a640934a99b25ceca8
serdilan <srdlnyl@gmail.com>
2022-01-22 05:51:40 +0300

    changed some

--- src/com/company/Test.java
+++ src/com/company/Test.java
@@ -1,10 +1,8 @@
 package com.company;
 
-public class Main {
-
+public class Test {
     public static void main(String[] args) {
-	// write your code here
+
         System.out.println("Hello");
-        System.out.println("slaw");
     }
 }
